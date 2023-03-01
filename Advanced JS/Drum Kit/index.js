c9511b40005273320db00ce6687ae57f46ef9f60
Radient12 <93489868+Radient12@users.noreply.github.com>
2023-03-01 23:53:10 +0200

    Added button animation

--- Advanced JS/Drum Kit/index.js
+++ Advanced JS/Drum Kit/index.js
@@ -5,7 +5,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
-        
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
@@ -51,4 +51,13 @@ function makeSound(key){
 
 document.addEventListener("keydown", function(event){
    makeSound(event.key);
+   buttonAnimation(event.key);
 });
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100)
+}
