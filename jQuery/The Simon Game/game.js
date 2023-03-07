06106a140d083416b59b4a8a3e514e07e1c1f94e
Lachezara <93489868+llachezara@users.noreply.github.com>
2023-03-07 18:27:07 +0200

    Add animations and start to the game
    Added animations to user clicks and detection of keyboard keys.

--- jQuery/The Simon Game/game.js
+++ jQuery/The Simon Game/game.js
@@ -2,6 +2,16 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+var started = false, level = 0;
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+
+    }
+});
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
 
@@ -10,20 +20,32 @@ function nextSequence() {
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    playSound(randomChosenColour);
+    playSound(randomChosenColor);
+
+    level++;
+    $("#level-title").text("Level " + level);
 }
-nextSequence();
+
 
 $(".btn").click(function () {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
 
-    console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 })
 
 function playSound(name) {
 
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+}
+
+function animatePress(currentColor) {
+
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
 }
